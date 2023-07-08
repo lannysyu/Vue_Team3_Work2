f04c01879ec241fe5eb421feb72a120f8c21bd1c
lannysyu <oopsjohn.77@gmail.com>
2023-07-09 03:22:33 +0800

    cart

---
+++ cartList.js
@@ -0,0 +1,26 @@
+const cartList = [
+    {
+        id: 1,
+        name: 'Bolognese Lasagne',
+        desc: '#01016',
+        price: 180,
+        stock: 5,
+        count: 1
+    },
+    {
+        id: 2,
+        name: 'Korean Gochujang Soup',
+        desc: '#02003',
+        price: 180,
+        stock: 5,
+        count: 1
+    },
+    {
+        id: 3,
+        name: 'Greek Style Fruit Salad',
+        desc: '#03010',
+        price: 180,
+        stock: 5,
+        count: 1
+    }
+]
---
+++ cartListWeek2.js
@@ -0,0 +1,18 @@
+const cartListWeek2 = [
+    {
+        id: 4,
+        name: 'Thai Green Curry',
+        desc: '#01010',
+        price: 180,
+        stock: 5,
+        count: 1
+    },
+    {
+        id: 5,
+        name: 'Chicken Braised in Red Winep',
+        desc: '#01008',
+        price: 180,
+        stock: 5,
+        count: 1
+    }
+]
