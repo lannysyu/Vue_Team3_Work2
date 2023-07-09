a6180ee315468c004c1a418f855254688b639866
lannysyu <oopsjohn.77@gmail.com>
2023-07-09 22:42:05 +0800

    update tab week2

--- cartList.js
+++ cartList.js
@@ -22,5 +22,21 @@ const cartList = [
         price: 180,
         stock: 5,
         count: 1
+    },
+    {
+        id: 4,
+        name: 'Thai Green Curry',
+        desc: '#01010',
+        price: 180,
+        stock: 5,
+        count: 1
+    },
+    {
+        id: 5,
+        name: 'Chicken Braised in Red Winep',
+        desc: '#01008',
+        price: 180,
+        stock: 5,
+        count: 1
     }
 ]
